5065e63375c619265a17e654d1556ee54b2666b1
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-09 17:54:14 +0300

    Initial code for server.ts file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,8 @@
 import express, { Request, Response } from "express";
+
+const app = express();
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
