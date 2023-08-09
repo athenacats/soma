1980ba1d9759ff590eda7d9aee15bd1b4a886dcc
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-09 15:54:24 +0300

    Change default font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
-import "./App.css";
-
 function App() {
   return (
-    <div>
+    <div className="d-flex flex-column vh-100">
       <header>Soma</header>
       <main></main>
       <footer>All Rights Reserved</footer>
