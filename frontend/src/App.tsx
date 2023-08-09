6b6e51925ba4621c901bceb92ac9cf11fd7b939e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-09 13:46:51 +0300

    Initialise app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>Soma</header>
+      <main></main>
+      <footer>All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
