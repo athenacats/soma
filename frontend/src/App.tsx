1abf7d345d6fd53ac53ec512d0e2b393efd407a5
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-09 17:09:18 +0300

    Add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
+
 function App() {
   return (
     <div className="d-flex flex-column vh-100">
-      <header>Soma</header>
+      <header>
+        <Navbar bg="light" variant="light" expand="lg">
+          <Container>
+            <NavbarBrand>Soma</NavbarBrand>
+          </Container>
+          <Nav>
+            <a href="cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main></main>
-      <footer>All Rights Reserved</footer>
+      <footer>
+        <div className="text-center">All Rights Reserved</div>
+      </footer>
     </div>
   );
 }
