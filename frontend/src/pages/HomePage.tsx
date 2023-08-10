b285dfc8a2b2272d6b2b3153b28b61f045b78719
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-10 10:34:46 +0300

    Set up types and functions to fetch data

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,35 @@
+import { Book } from "../types/Book";
+
+type State = {
+  books: Book[];
+  loadingBooks: boolean;
+  error: string;
+};
+
+type Action =
+  | { type: "FETCH_REQUEST" }
+  | { type: "FETCH_SUCCESS"; payload: Book[] }
+  | { type: "FETCH_FAIL"; payload: string };
+
+const initialState: State = {
+  books: [],
+  loadingBooks: true,
+  error: "",
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, books: action.payload };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 export default function HomePage() {
   return <></>;
 }
