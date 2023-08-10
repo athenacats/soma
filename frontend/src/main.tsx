b957cea1a4e7817c35a98f229425944581eeef2e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-10 16:28:34 +0300

    Create apiClient and install react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,11 +11,10 @@ import App from "./App.tsx";
 import "./index.css";
 import HomePage from "./pages/HomePage.tsx";
 import BookPage from "./pages/BookPage.tsx";
-import axios from "axios";
-import { HelmetProvider } from "react-helmet-async";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:4000" : "/";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,10 +27,15 @@ const router = createBrowserRouter(
   )
 );
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
