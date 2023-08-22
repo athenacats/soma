1f4c126fbf6501bbe330686b9cae04a85be1445b
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-22 11:15:06 +0300

    Make navbarbrand linkable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { Button, Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import { Store } from "./Store";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   const {
@@ -21,7 +22,9 @@ function App() {
       <header>
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <NavbarBrand>Soma</NavbarBrand>
+            <LinkContainer to="/">
+              <NavbarBrand>Soma</NavbarBrand>
+            </LinkContainer>
           </Container>
           <Nav>
             <Button variant={mode} onClick={switchModeHandler}>
