1b93113ebf1d890f3a2a41aafd0306c75d0596cc
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-22 15:16:27 +0300

    Separate the newbooksthisweek to own file

---
+++ frontend/src/components/NewBooksThisWeek.tsx
@@ -0,0 +1,30 @@
+import { getError } from "../utils";
+import { ApiError } from "../types/ApiError";
+import { Col, Row } from "react-bootstrap";
+import LoadingMessage from "../components/LoadingMessage";
+import MessageBox from "../components/MessageBox";
+import BookItem from "../components/BookItem";
+import { useGetBooksQuery } from "../hooks/bookHooks";
+
+export default function NewBooksThisWeek() {
+  const { isLoading, error, data: books } = useGetBooksQuery();
+
+  return isLoading ? (
+    <LoadingMessage />
+  ) : error ? (
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
+  ) : (
+    <div className="body-container">
+      <div>
+        <h4 className="text-center ">New Books This Week</h4>
+        <Row>
+          {books!.slice(0, 8).map((book, index) => (
+            <Col key={index} sm={6} md={4} lg={3}>
+              <BookItem book={book} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,32 +1,13 @@
-import { getError } from "../utils";
-import { ApiError } from "../types/ApiError";
-import { Col, Row } from "react-bootstrap";
-import LoadingMessage from "../components/LoadingMessage";
-import MessageBox from "../components/MessageBox";
-import BookItem from "../components/BookItem";
 import { Helmet } from "react-helmet-async";
-import { useGetBooksQuery } from "../hooks/bookHooks";
+import NewBooksThisWeek from "../components/NewBooksThisWeek";
 
 export default function HomePage() {
-  const { isLoading, error, data: books } = useGetBooksQuery();
-
-  return isLoading ? (
-    <LoadingMessage />
-  ) : error ? (
-    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
-  ) : (
+  return (
     <div className="body-container">
-      <h4 className="text-center ">Life is dull without books...</h4>
-      <Row>
-        <Helmet>
-          <title>Soma</title>
-        </Helmet>
-        {books!.slice(0, 8).map((book, index) => (
-          <Col key={index} sm={6} md={4} lg={3}>
-            <BookItem book={book} />
-          </Col>
-        ))}
-      </Row>
+      <Helmet>
+        <title>Soma</title>
+      </Helmet>
+      <NewBooksThisWeek></NewBooksThisWeek>
     </div>
   );
 }
