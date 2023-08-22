1f57a5c96b35dc22a34961621055fafd8c1711e3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-22 16:15:49 +0300

    Add dropdown menu with some categories

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import { useContext, useEffect } from "react";
-import { Button, Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import {
+  Container,
+  Nav,
+  NavDropdown,
+  Navbar,
+  NavbarBrand,
+} from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
 import { Store } from "./Store";
 import { LinkContainer } from "react-router-bootstrap";
 
@@ -20,22 +26,44 @@ function App() {
   return (
     <div className="d-flex flex-column vh-100">
       <header>
-        <Navbar variant={mode} expand="lg">
+        <Navbar variant={mode} expand="lg" className="d-flex  p-3 ">
           <Container>
             <LinkContainer to="/">
               <NavbarBrand>Soma</NavbarBrand>
             </LinkContainer>
           </Container>
           <Nav>
-            <Button variant={mode} onClick={switchModeHandler}>
+            <Link
+              to="#"
+              className="nav-link header-link"
+              onClick={switchModeHandler}
+            >
               <i className={mode === "light" ? "fa fa-sun" : "fa fa-moon"}></i>
-            </Button>
-            <a href="cart" className="nav-link">
-              Cart
-            </a>
+            </Link>
+
             <a href="/signin" className="nav-link">
-              Sign In
+              <i className="far fa-user"></i>
             </a>
+            <NavDropdown className="header-link" title="Categories">
+              <LinkContainer to="/fiction">
+                <NavDropdown.Item>Fiction</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/mystery&crime">
+                <NavDropdown.Item>Mystery & Crime</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/romance">
+                <NavDropdown.Item>Romance</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/science&tech">
+                <NavDropdown.Item>Science & Technology</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/scifi&fantasy">
+                <NavDropdown.Item>Science Fiction & Fantasy</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/teens&ya">
+                <NavDropdown.Item>Teens & YA</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
           </Nav>
         </Navbar>
       </header>
