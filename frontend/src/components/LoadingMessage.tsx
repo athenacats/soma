223bebdd0565d70c300f92521f7d344505494b9c
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-24 17:30:50 +0300

    Change spinner color, size, location

--- frontend/src/components/LoadingMessage.tsx
+++ frontend/src/components/LoadingMessage.tsx
@@ -2,8 +2,17 @@ import { Spinner } from "react-bootstrap";
 
 export default function LoadingMessage() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Spinner
+        animation="border"
+        role="status"
+        style={{ color: "#f5ad43", height: "4rem", width: "4rem" }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
