bacdb2fb47737b55d1739d697c7640ace3cf1b62
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-25 16:48:16 +0300

    Create UserModel

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,16 @@
+import { prop, getModelForClass, modelOptions } from "@typegoose/typegoose";
+
+@modelOptions({ schemaOptions: { timestamps: true } })
+export class User {
+  public _id?: string;
+  @prop({ required: true })
+  public name!: string;
+  @prop({ required: true })
+  public email!: string;
+  @prop({ required: true })
+  public password!: string;
+  @prop({ required: true, default: false })
+  public isAdmin!: boolean;
+}
+
+export const UserModel = getModelForClass(User);
