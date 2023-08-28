e58cdde9e6358fd98a944d8c2e18e6f9b9ddc344
athenacats <124017569+athenacats@users.noreply.github.com>
2023-08-28 13:08:02 +0300

    Create sign up page

---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,106 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Store } from "../Store";
+import { useSignUpMutation } from "../hooks/userHook";
+import { getError } from "../utils";
+import { ApiError } from "../types/ApiError";
+import { toast } from "react-toastify";
+import { Button, Container, Form } from "react-bootstrap";
+import { Helmet } from "react-helmet-async";
+import LoadingMessage from "../components/LoadingMessage";
+
+export const SignUpPage = () => {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const redirectUrl = new URLSearchParams(search).get("redirect");
+  const redirect = redirectUrl ? redirectUrl : "/";
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const { mutateAsync: signup, isLoading } = useSignUpMutation();
+
+  const submitHandler = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+      return;
+    }
+    try {
+      const data = await signup({
+        name,
+        email,
+        password,
+      });
+      dispatch({ type: "USER_SIGNIN", payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      navigate(redirect);
+    } catch (err) {
+      toast.error(getError(err as ApiError));
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign Up</title>
+      </Helmet>
+      <h1 className="my-3">Sign Up</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div className="mb-3">
+          <Button disabled={isLoading} type="submit">
+            Sign Up
+          </Button>
+          {isLoading && <LoadingMessage />}
+        </div>
+        <div className="mb-3">
+          Already have an account?{" "}
+          <Link to={`/signin?redirect=${redirect}`}>SIgn In</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+};
