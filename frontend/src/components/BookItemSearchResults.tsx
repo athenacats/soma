d691b8a9d85e2f5685fb909773c66233ec4539e2
athenacats <124017569+athenacats@users.noreply.github.com>
2024-01-09 18:49:29 +0300

    Spinner loader when image is loading for search results

--- frontend/src/components/BookItemSearchResults.tsx
+++ frontend/src/components/BookItemSearchResults.tsx
@@ -1,11 +1,37 @@
-import { Card } from "react-bootstrap";
+import { Card, Spinner } from "react-bootstrap";
 import { Book } from "../types/Book";
 import Rating from "./Rating";
+import { useState } from "react";
 
 export default function BookItemSearchResults({ book }: { book: Book }) {
+  const [imageIsLoading, setImageIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setImageIsLoading(false);
+  };
+
   return (
     <Card className="mb-3" style={{ cursor: "pointer" }}>
-      <img src={book.image} className="card-img-top" alt={book.name} />
+      {imageIsLoading && (
+        <Spinner
+          animation="border"
+          role="status"
+          style={{
+            color: "#f5ad43",
+            height: "2rem",
+            width: "2rem",
+            marginTop: "1rem",
+          }}
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      <img
+        src={book.image}
+        className="card-img-top"
+        alt={book.name}
+        onLoad={handleImageLoad}
+      />
 
       <Card.Body>
         <Card.Title>{book.name}</Card.Title>
