cd3f2a2085dc9923b302339de46046b0962b497b
athenacats <124017569+athenacats@users.noreply.github.com>
2024-01-09 16:57:58 +0300

    Change component display to avoid network calls

--- frontend/src/pages/BookPage.tsx
+++ frontend/src/pages/BookPage.tsx
@@ -6,8 +6,8 @@ import MessageBox from "../components/MessageBox";
 import { getError } from "../utils";
 import { ApiError } from "../types/ApiError";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import BookItem from "../components/BookItem";
 import { Book } from "../types/Book";
+import BookItemSearchResults from "../components/BookItemSearchResults";
 
 export default function BookPage() {
   const params = useParams();
@@ -83,7 +83,7 @@ export default function BookPage() {
         {uniqueBooks.length > 0 ? (
           uniqueBooks.slice(0, 3).map((book, index) => (
             <Col key={index} sm={6} md={4} lg={3}>
-              <BookItem book={book} />
+              <BookItemSearchResults book={book} />
               <Container className="d-flex justify-content-evenly flex-wrap">
                 <Button
                   className="mt-2 btn btn-primary btn-sm"
