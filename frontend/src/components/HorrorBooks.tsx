6b255cd80cd3f46e4a182b619f57da753ab59dc3
athenacats <124017569+athenacats@users.noreply.github.com>
2024-01-16 11:49:25 +0300

    Css for horror page pagination

--- frontend/src/components/HorrorBooks.tsx
+++ frontend/src/components/HorrorBooks.tsx
@@ -1,13 +1,99 @@
 import { getError } from "../utils";
 import { ApiError } from "../types/ApiError";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import LoadingMessage from "../components/LoadingMessage";
 import MessageBox from "../components/MessageBox";
 import BookItem from "../components/BookItem";
 import { useGetHorrorBooksQuery } from "../hooks/bookHooks";
+import { useState } from "react";
+
+const ITEMS_PER_PAGE = 10;
 
 export default function HorrorBooks() {
   const { isLoading, error, data: books } = useGetHorrorBooksQuery();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const totalPages = Math.ceil(books?.length || 0) / ITEMS_PER_PAGE;
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = Array.from(
+      { length: totalPages },
+      (_, index) => index + 1
+    );
+
+    return (
+      <div className="text-center mt-3">
+        <Button
+          style={{
+            border: "1px solid #fff",
+            marginBottom: "0.5rem",
+            textShadow: "  1px 1px 2px rgb(92, 62, 17)",
+          }}
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </Button>
+        {pageNumbers.map((page) => (
+          <Button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            className={page === currentPage ? "active" : ""}
+            style={{
+              marginLeft: "0.5rem",
+              border: "1px solid #fff",
+              marginBottom: "0.5rem",
+              textShadow: "  1px 1px 2px rgb(92, 62, 17)",
+            }}
+          >
+            {page}
+          </Button>
+        ))}
+        <Button
+          style={{
+            border: "1px solid #fff",
+            marginBottom: "0.5rem",
+            textShadow: "  1px 1px 2px rgb(92, 62, 17)",
+          }}
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </Button>
+      </div>
+    );
+  };
+
+  const renderBooks = () => {
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+
+    return (
+      <Row>
+        {books?.slice(startIndex, endIndex).map((book, index) => (
+          <Col key={index} sm={12} md={6} lg={6}>
+            <BookItem book={book} />
+          </Col>
+        ))}
+      </Row>
+    );
+  };
 
   return isLoading ? (
     <LoadingMessage />
@@ -17,13 +103,8 @@ export default function HorrorBooks() {
     <div className="body-container">
       <div>
         <h1 className="text-center mb-4">Best Selling Horror Books</h1>
-        <Row>
-          {books!.slice(0, 90).map((book, index) => (
-            <Col key={index} sm={12} md={6} lg={6}>
-              <BookItem book={book} />
-            </Col>
-          ))}
-        </Row>
+        {renderBooks()}
+        {renderPagination()}
       </div>
     </div>
   );
